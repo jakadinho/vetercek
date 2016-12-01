38326a53a3155f27832ada8976329cac3de0035c
Jaka Kukovič <jakakukovic@gmail.com>
2016-12-01 09:51:34 +0100

    template matching

--- detect_shape.py
+++ detect_shape.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python
 import cv2
-import numpy as np;
+import numpy as np
+from matplotlib import pyplot as plt
 
-# load the image
-image = cv2.imread('liz.jpg')
-# find all the 'orange' shapes in the image
-upper = np.array([80, 80, 80])
-lower = np.array([0, 0, 0])
+img_rgb = cv2.imread('sample.png')
+img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
-shapeMask = cv2.inRange(image, lower, upper)
-new_file=open('filtered.jpg','wb')
+directions=['n','ne','s']
+for direction in directions:   
+	template = cv2.imread(direction+'.png',0)
+	w, h = template.shape[::-1]
 
-#cv2.imshow("Keypoints", shapeMask)
-#cv2.waitKey(0)
+	count=0 
+	res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+	threshold = 0.85
+	loc = np.where( res >= threshold)
+	for pt in zip(*loc[::-1]):
+		cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+		count=count+1
+	#cv2.imwrite('res.png',img_rgb)
 
-cv2.imwrite('filtered.jpg',shapeMask)
-new_file.close()
+	print 'dir', direction, count
+
+	# Show blobs
+	cv2.imshow("Keypoints", img_rgb)
+	cv2.waitKey(0)
+
